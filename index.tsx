e7de6b032ec21a64006b184fa55a036be5d4357d
Tyson Skiba <tyson@skiba.com.au>
2021-09-09 20:48:27 +1000

    feat: remove html node after callback is run
    This ensures that mounting with react 17 and react 18+ result in the same hierarchy

--- index.tsx
+++ index.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 
-interface CallbackProps {
-    callback?: () => void;
+type Callback = (() => void) | undefined;
+
+interface CallbackProps  {
+    callback?: Callback;
     children: React.ReactChild | React.ReactNodeArray;
+}
+
+const onMount = (element: HTMLDivElement | null, callback: Callback) => {
+    if (callback) callback();
+    if (element && element.parentElement) element.parentElement.removeChild(element);
 }
 
 export const CallbackWrapper: React.FC<CallbackProps> = ({
     callback,
     children
-}) => callback
-    ? <div ref={callback}>{ children }</div>
-    : <>{ children }</>
+}) => (
+    <>
+        { children }
+        <div
+            ref={element => onMount(element, callback)}
+            style={{ display: 'none' }}
+        />
+    </>
+)
